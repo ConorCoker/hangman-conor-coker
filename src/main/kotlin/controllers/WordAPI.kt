169d7c7c726fc678aaca99a87797ddc47c6fe1ac
Conor Coker <conorcoker@gmail.com>
2023-03-31 17:24:07 +0100

    Added gradle plugins/dependencies for dokka worked on WordAPI added methods to allow a player to choose a word(If setting up game for friend) or get a random word to guess the gameword is converted to a char array and another char array of _ _ underscores is created to represent the unknown letters of the word fleshed out menu system in Main

--- src/main/kotlin/controllers/WordAPI.kt
+++ src/main/kotlin/controllers/WordAPI.kt
@@ -1,10 +1,16 @@
 package controllers
 
 import models.Word
+import utils.Utils
+import java.util.Random
 
 class WordAPI {
 
     private var words = ArrayList<Word>()
+    private var underscores = ArrayList<Char>()
+    private lateinit var gameWord: Word
+    private lateinit var gameWordAsCharArray: CharArray
+
 
 
     fun addWord(word: Word) = words.add(word)
@@ -12,8 +18,40 @@ class WordAPI {
 
     fun numberOfWords() = words.size
 
-    fun removeWordByIndex(indexToRemove:Int) = words.removeAt(indexToRemove)
+    fun removeWordByIndex(indexToRemove: Int) = words.removeAt(indexToRemove)
+    fun showAllWords(): String {
+        return words.joinToString(separator = "\n") {
+            words.indexOf(it)
+                .toString() + " " + it.word + ", Max Guesses: " + it.maxGuesses + ", Difficulty: " + it.difficulty
+        }
+    }
+
+    fun getRandomWord(difficulty: Int): Word? {
+        val random = Random()
+        val filteredList = when (difficulty) {
+            0 -> words
+            else -> words.filter { it.difficulty == difficulty }
+        }
+        return if (filteredList.isEmpty()) {
+            null
+        } else filteredList[random.nextInt(filteredList.size)]
+    }
+
+    fun getWordByIndex(index: Int): Word? {
+        return if (Utils.isValidIndex(index, words)) {
+            words[index]
+        } else null
+    }
 
 
+    fun startGame(word:Word){
+        gameWord = word
+        gameWordAsCharArray = gameWord.word.toCharArray()
+        gameWordAsCharArray.forEach { _ -> underscores.add('_') }
+    }
+
+    fun printGameWord() = gameWordAsCharArray.joinToString { it.toString() }
+
+    fun printHidden() = underscores.joinToString { it.toString() }
 
 }
