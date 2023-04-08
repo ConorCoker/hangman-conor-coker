bffa4527ad2881caf76ad05aca41a3af37bcff5c
Conor Coker <conorcoker@gmail.com>
2023-04-08 18:23:43 +0100

    completed listing methods and leaderboard method for main

--- src/main/kotlin/controllers/PlayerAPI.kt
+++ src/main/kotlin/controllers/PlayerAPI.kt
@@ -35,10 +35,28 @@ class PlayerAPI {
     }
 
     fun listLoggedInPlayers():String {
-        return players.filter { it.loggedIn }.joinToString(separator = ", ") {
-            it.name
+        var str = "No players in system!"
+        if (players.isNotEmpty()){
+            str = "No logged in players!"
+            val list = players.filter { it.loggedIn }
+            if (list.isNotEmpty()){
+                str = list.joinToString(separator = ", ") {
+                    it.name
+                }
+            }
         }
+       return str
     }
+
+    fun listAllPlayers():String{
+        var str = "No players in system!"
+        if (players.isNotEmpty()){
+            str = players.joinToString(separator = ", ") { it.name  }
+        }
+        return str
+    }
+
+    fun getLeaderboard() = players.joinToString (separator = "\n"){"|Name: ${it.name}|Games Played: ${it.gamesPlayed}|Wins: ${it.wins}|Losses: ${it.losses}|Highest Score: ${it.highestScore}|"}
 
 
 }
--- src/main/kotlin/controllers/WordAPI.kt
+++ src/main/kotlin/controllers/WordAPI.kt
@@ -35,9 +35,9 @@ class WordAPI {
         return if (words.isNotEmpty()) {
             val random = Random()
             val filteredList = words.filter { difficulty == it.difficulty }
-            if (filteredList.isNotEmpty()){
+            if (filteredList.isNotEmpty()) {
                 filteredList[random.nextInt(filteredList.size)]
-            }else words[random.nextInt(words.size)]
+            } else words[random.nextInt(words.size)]
         } else null
     }
 
@@ -48,110 +48,113 @@ class WordAPI {
         } else null
     }
 
-//    init {
-//        words.add(
-//            Word(
-//                "computer",
-//                "An electronic device for storing and processing data",
-//                3,
-//                "An electronic device that can receive, store, and process data, and produce a result or output based on a stored program or sequence of instructions."
-//            )
-//        )
-//        words.add(
-//            Word(
-//                "television",
-//                "A system for transmitting visual images and sound that are reproduced on screens",
-//                2,
-//                "A system for transmitting visual images and sound that are reproduced on screens, chiefly used to broadcast programs for entertainment, information, and education."
-//            )
-//        )
-//        words.add(
-//            Word(
-//                "restaurant",
-//                "A place where people pay to sit and eat meals that are cooked and served on the premises",
-//                2,
-//                "A place where people go to eat meals that are cooked and served on the premises, typically paying for the food and service."
-//            )
-//        )
-//        words.add(
-//            Word(
-//                "mountain",
-//                "A large natural elevation of the earth's surface rising abruptly from the surrounding level",
-//                2,
-//                "A large natural elevation of the earth's surface rising abruptly from the surrounding level; a large steep hill."
-//            )
-//        )
-//        words.add(
-//            Word(
-//                "guitar",
-//                "A stringed musical instrument played with a plectrum or finger, typically having six strings",
-//                2,
-//                "A musical instrument played with a plectrum or the fingers, typically having six strings tuned to E, A, D, G, B, and E."
-//            )
-//        )
-//        words.add(
-//            Word(
-//                "ocean",
-//                "A very large expanse of sea, in particular each of the main areas into which the sea is divided geographically",
-//                2,
-//                "A very large expanse of sea, especially each of the main areas into which the sea is divided geographically."
-//            )
-//        )
-//        words.add(
-//            Word(
-//                "smartphone",
-//                "A mobile phone that performs many of the functions of a computer, typically having a touchscreen interface, Internet access, and an operating system capable of running downloaded apps",
-//                4,
-//                "A mobile phone that has advanced features beyond traditional mobile phones, such as the ability to access the Internet, run software applications (apps), and typically a touchscreen interface."
-//            )
-//        )
-//        words.add(
-//            Word(
-//                "university",
-//                "A high-level educational institution in which students study for degrees and academic research is done",
-//                2,
-//                "A high-level educational institution where students study for degrees and academic research is done."
-//            )
-//        )
-//        words.add(
-//            Word(
-//                "aviation",
-//                "The flying or operating of aircraft",
-//                3,
-//                "The flying or operating of aircraft, or the design, construction, and maintenance of aircraft."
-//            )
-//        )
-//        words.add(
-//            Word(
-//                "soccer",
-//                "A game played by two teams of eleven players with a round ball that may not be touched with the hands or arms during play except by the goalkeeper",
-//                2,
-//                "A game played by two teams of eleven players with a round ball that may not be touched with the hands or arms during play except by the goalkeeper, the objective of which is to score goals by kicking or heading the ball into the opposing team's goal."
-//            )
-//        )
-//        words.add(
-//            Word(
-//                "medicine",
-//                "The science or practice of the diagnosis, treatment, and prevention of disease",
-//                3,
-//                "The science or practice of the diagnosis, treatment, and prevention of disease, as well as the maintenance of good health."
-//            )
-//        )
-//        words.add(
-//            Word(
-//                "astronomy",
-//                "The branch of science that deals with celestial objects, space, and the physical universe as a whole",
-//                4,
-//                "The branch of science that deals with celestial objects, space, and the physical universe as a whole."
-//            )
-//        )
-//        words.add(
-//            Word(
-//                "biology",
-//                "The study of living organisms, divided into many specialized fields that cover their morphology, physiology, anatomy, behavior, origin, and distribution",
-//                3,
-//                "The study of living organisms, including their morphology, physiology, anatomy, behavior, origin, and distribution."
-//            )
-//        )
-//    }
+    fun listAllSolvedWords() = words.filter { it.solved }.joinToString(separator = "\n") { it.word }
+
+    fun loadWords() {
+        words.add(
+            Word(
+                "computer",
+                "An electronic device for storing and processing data",
+                3,
+                "An electronic device that can receive, store, and process data, and produce a result or output based on a stored program or sequence of instructions."
+            )
+        )
+        words.add(
+            Word(
+                "television",
+                "A system for transmitting visual images and sound that are reproduced on screens",
+                2,
+                "A system for transmitting visual images and sound that are reproduced on screens, chiefly used to broadcast programs for entertainment, information, and education."
+            )
+        )
+        words.add(
+            Word(
+                "restaurant",
+                "A place where people pay to sit and eat meals that are cooked and served on the premises",
+                2,
+                "A place where people go to eat meals that are cooked and served on the premises, typically paying for the food and service."
+            )
+        )
+        words.add(
+            Word(
+                "mountain",
+                "A large natural elevation of the earth's surface rising abruptly from the surrounding level",
+                2,
+                "A large natural elevation of the earth's surface rising abruptly from the surrounding level; a large steep hill."
+            )
+        )
+        words.add(
+            Word(
+                "guitar",
+                "A stringed musical instrument played with a plectrum or finger, typically having six strings",
+                2,
+                "A musical instrument played with a plectrum or the fingers, typically having six strings tuned to E, A, D, G, B, and E."
+            )
+        )
+        words.add(
+            Word(
+                "ocean",
+                "A very large expanse of sea, in particular each of the main areas into which the sea is divided geographically",
+                2,
+                "A very large expanse of sea, especially each of the main areas into which the sea is divided geographically."
+            )
+        )
+        words.add(
+            Word(
+                "smartphone",
+                "A mobile phone that performs many of the functions of a computer, typically having a touchscreen interface, Internet access, and an operating system capable of running downloaded apps",
+                4,
+                "A mobile phone that has advanced features beyond traditional mobile phones, such as the ability to access the Internet, run software applications (apps), and typically a touchscreen interface."
+            )
+        )
+        words.add(
+            Word(
+                "university",
+                "A high-level educational institution in which students study for degrees and academic research is done",
+                2,
+                "A high-level educational institution where students study for degrees and academic research is done."
+            )
+        )
+        words.add(
+            Word(
+                "aviation",
+                "The flying or operating of aircraft",
+                3,
+                "The flying or operating of aircraft, or the design, construction, and maintenance of aircraft."
+            )
+        )
+        words.add(
+            Word(
+                "soccer",
+                "A game played by two teams of eleven players with a round ball that may not be touched with the hands or arms during play except by the goalkeeper",
+                2,
+                "A game played by two teams of eleven players with a round ball that may not be touched with the hands or arms during play except by the goalkeeper, the objective of which is to score goals by kicking or heading the ball into the opposing team's goal."
+            )
+        )
+        words.add(
+            Word(
+                "medicine",
+                "The science or practice of the diagnosis, treatment, and prevention of disease",
+                3,
+                "The science or practice of the diagnosis, treatment, and prevention of disease, as well as the maintenance of good health."
+            )
+        )
+        words.add(
+            Word(
+                "astronomy",
+                "The branch of science that deals with celestial objects, space, and the physical universe as a whole",
+                4,
+                "The branch of science that deals with celestial objects, space, and the physical universe as a whole."
+            )
+        )
+        words.add(
+            Word(
+                "biology",
+                "The study of living organisms, divided into many specialized fields that cover their morphology, physiology, anatomy, behavior, origin, and distribution",
+                3,
+                "The study of living organisms, including their morphology, physiology, anatomy, behavior, origin, and distribution."
+            )
+        )
     }
+}
+
